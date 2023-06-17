64b8c4ab6765c764977972865aaff11ffea32477
Vasilii Karmanov <oldheffalump1@gmail.com>
2023-06-17 19:51:19 +0300

    HomeWork 2.3 add nev fields in class Post add class VideoAttacment AudioAttacment PhotoAttacment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,100 @@
 package ru.netology
 
 
-data class Post (
+data class Post(
     var id: Int,
-    var likes: Likes = Likes(0),
-    var can_edit: Boolean = true,
-    var post_type: String = "post",
+    var owner_id: Likes = Likes(0),
+    var from_id: Int = 0,
+    var created_by: Int = 0,
+    var date: Int = 0,
     var text: String = "",
+    var reply_owner_id: Int = 0,
     var reply_post_id: Int = 0,
-    var copy_history: Array<String> = emptyArray(),
-    var copuright: String = "origins",
     var friends_only: Boolean = false,
-    var is_favorite: Boolean = false
+    var comments: Object = Object(),
+    var copuright: String = "origins",
+    var likes: Likes = Likes(0),
+    var repost: Repost = Repost(),
+    var views: Views = Views(),
+    var post_type: String = "post",
+    var post_sourse: PostSourse = PostSourse(),
+    var copy_history: Array<String> = emptyArray(),
+    var geo: Geo = Geo(),
+    var singer_id: Int = 0,
+    var can_pin: Boolean = true,
+    var can_delete: Boolean = true,
+    var can_edit: Boolean = true,
+    var is_pinned: Boolean = false,
+    var maker_as_ads: Boolean = false,
+    var is_favorite: Boolean = false,
+    var postoned_id: Int = 0
+)
+
+data class Geo(
+    var type: String = "",
+    var coordinates: String = "",
+    var place: Place = Place(),
+
+)
+
+data class Place(
+    var description: String = "none"
+)
 
 
-
+data class PostSourse(
+    var count: Int = 0
 )
 
 data class Likes (
     var count: Int
 )
+
+data class Object(
+    var count: Int = 0,
+    var can_post: Boolean = true,
+    var groups_can_post: Boolean = true,
+    var can_close: Boolean = true,
+    var can_open: Boolean = true
+)
+
+data class Repost(
+    var count: Int = 0,
+    var user_reposted: Boolean = false
+)
+
+data class Views(
+    var count: Int = 0
+)
+
+interface Attachment{
+    val type: String
+}
+
+data class Video(
+    var id: Int,
+    var owner_id: Int,
+    var title: String,
+    var duration: Int
+)
+data class VideoAttacment(override val type: String = "video", val video: Video ): Attachment
+
+data class Audio(
+    var id: Int,
+    var owner_id: Int,
+    var title: String,
+    var duration: Int
+)
+data class AudioAttacment(override val type: String = "audio", val audio: Audio ): Attachment
+
+data class Photo(
+    var id: Int,
+    var owner_id: Int,
+    var photo_130: String,
+    var photo_604: String
+)
+data class PhotoAttacment(override val type: String = "photo", val photo: Photo ): Attachment
+
 
 
 object WallService{
@@ -75,4 +150,5 @@ fun main() {
     WallService.update(Post(2, Likes(123)))
     WallService.print()
 
+    println(VideoAttacment("video", Video(1,2, "Granny", 30) ))
 }
